dd144fef45bd1ad5784ce48c2a69524ea7bdc78b
Louis Cortes <cortes.lou@gmail.com>
2021-03-19 17:35:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -302,7 +302,7 @@ return length(ua-ba*h);}
     //float dp = smoothstep(0.003,0.001,li(uv,m+clamp((m2-0.5)*-1.,-0.2,0.2),m));
     //float d7 = mix(d6,1.-d6,dp);
     vec2 tb2 = texture2D(uTarget,uv+(uv-0.5)*0.03).xy;
-    float tb3 = sin(tb2.x*(fract(time)*5.+7.));
+    float tb3 = sin(tb2.x*(fract(time*2.)*5.+7.));
     float d8 = max(mix(d6,0.,tb3),mix(1.-d6,0.,tb3)*0.2);
   //float d9 = max(1.-d6,tb2.y*0.9);
         gl_FragColor = vec4(smoothstep(0.,1.,d8),0.,0.,fm);
@@ -677,8 +677,9 @@ function sons() {
     var time = new Date().getMilliseconds() / 1000.;
     //ocs.type = 'sine';
     //osc.frequency.value = (1.-Math.pow(fract(time*3.),0.7))*100.*pointers[0].texcoordY;
-    osc.frequency.value = (pointers[0].texcoordX)*100.+(pointers[0].texcoordY)*100.;
-    vol.gain.value =(1.-Math.pow(fract(time),0.5))*3.+0.2;
+    var f1 = (1.-Math.pow(fract(time*2.),(pointers[0].texcoordX)));
+    osc.frequency.value = ((pointers[0].texcoordY)*50.);
+    vol.gain.value =f1*50.+1.;
 }
 
 osc.connect(vol);
